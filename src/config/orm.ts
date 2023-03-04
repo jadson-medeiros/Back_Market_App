5cd92bf49bc7d7378cbdeec113c21813fa07299c
jadson-medeiros <developer.medeiros@gmail.com>
2023-03-03 23:38:04 -0300

    added begin of the project

---
+++ src/config/orm.ts
@@ -0,0 +1,12 @@
+import path from 'path';
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+
+const options: TypeOrmModuleOptions = {
+  type: 'sqlite',
+  database: 'data/marketapp.db',
+  logging: true,
+  entities: [path.resolve(__dirname, '..', 'db', 'models', '*')],
+  migrations: [path.resolve(__dirname, '..', 'db', 'migrations', '*')],
+};
+
+module.exports = options;
