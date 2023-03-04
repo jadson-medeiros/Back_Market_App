2d6413e5b6e0c13d6f631df6c5c5496fd24910ac
jadson-medeiros <developer.medeiros@gmail.com>
2023-03-04 17:21:17 -0300

    add migration and ormconfig file

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSource } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import * as ormOptions from './config/ormconfig';
 
 @Module({
-  imports: [],
+  imports: [DataSource],
   controllers: [AppController],
   providers: [AppService],
 })
